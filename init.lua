eb159c448ba65474fdebd1e8e9f53c0114085e30
Trifalic47 <r00tgrep99@gmail.com>
2026-01-31 00:03:46 +0530

    Changing colorscheme and adding colorcolumn

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = " "
 vim.opt.guicursor = "n-v-c-i:block"
 vim.wo.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 2     -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 2  -- Size of an indentation
+vim.opt.softtabstop = 2 -- Backspace deletes a full indent
+
 
 require("keymaps")
 
@@ -12,27 +17,33 @@ vim.g.mapleader = " "
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
+-- Sets the column at the 80th character
+vim.opt.colorcolumn = "125"
+
+-- To make it subtle like his (not a bright red line)
+-- This sets the background of the column to a very dark grey/black
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#161616" })
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,122 +1,27 @@
---[[
 return {
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000, -- Load this before other plugins
-		config = function()
-			require("rose-pine").setup({
-				variant = "main", -- options: 'main', 'moon', or 'dawn'
-				dark_variant = "main",
-				dim_inactive_windows = false,
-				extend_background_behind_borders = true,
-
-				enable = {
-					terminal = true,
-					legacy_highlights = true,
-				},
-
-				styles = {
-					bold = true,
-					italic = true,
-					transparency = true, -- Set to true if you want a see-through background
-				},
-			})
-
-			-- Set the colorscheme
-			vim.cmd("colorscheme rose-pine")
-		end,
-	},
-}
---]]
-function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-return {
-
-	{
-		"erikbackman/brightburn.vim",
-	},
-
-	{
-		"folke/tokyonight.nvim",
+		"vague2k/vague.nvim",
 		lazy = false,
-		opts = {},
+		priority = 1000,
 		config = function()
-			ColorMyPencils()
-		end
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		name = "gruvbox",
-		config = function()
-			require("gruvbox").setup({
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = false,
-				bold = true,
-				italic = {
-					strings = false,
-					emphasis = false,
-					comments = false,
-					operators = false,
-					folds = false,
+			require("vague").setup({
+				transparent = true, -- This makes it inherit your terminal's black background
+				style = {
+					boolean = "bold",
+					number = "none",
+					float = "none",
+					error = "none",
+					comments = "italic", -- Prime's setup is usually clean, but you can toggle this
+					conditionals = "none",
+					functions = "none",
+					headings = "bold",
+					operators = "none",
+					strings = "none",
+					variables = "none",
+					keywords = "none",
 				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				invert_intend_guides = false,
-				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = false,
 			})
+			vim.cmd.colorscheme("vague")
 		end,
 	},
-	{
-		"folke/tokyonight.nvim",
-		config = function()
-			require("tokyonight").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-				transparent = true, -- Enable this to disable setting the background color
-				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-				styles = {
-					-- Style to be applied to different syntax groups
-					-- Value is any valid attr-list value for `:help nvim_set_hl`
-					comments = { italic = false },
-					keywords = { italic = false },
-					-- Background styles. Can be "dark", "transparent" or "normal"
-					sidebars = "dark", -- style for sidebars, see below
-					floats = "dark", -- style for floating windows
-				},
-			})
-		end
-	},
-
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require('rose-pine').setup({
-				disable_background = true,
-				styles = {
-					italic = false,
-				},
-			})
-
-			ColorMyPencils();
-		end
-	},
-
-
 }
