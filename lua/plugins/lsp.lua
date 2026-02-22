55b1e133dbd6d1e9e204b6da7be2d6d28bbc59dd
Trifalic47 <r00tgrep99@gmail.com>
2026-02-22 20:11:45 +0530

    Fixing bugs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,22 @@
 return {
-  'neovim/nvim-lspconfig',
-  lazy = false,
-  dependencies = { 'hrsh7th/cmp-nvim-lsp' },
-  config = function()
-    -- Define the variable properly
-    local lspconfig = require('lspconfig')
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	'neovim/nvim-lspconfig',
+	lazy = false,
+	dependencies = { 'hrsh7th/cmp-nvim-lsp' },
+	config = function()
+		-- Define the variable properly
+		local lspconfig = require('lspconfig')
+		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-    lspconfig.pyright.setup({ capabilities = capabilities })
-    lspconfig.ts_ls.setup({ capabilities = capabilities })
-    lspconfig.clangd.setup({ capabilities = capabilities })
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = { globals = { 'vim' } },
-        },
-      },
-    })
-  end,
+		lspconfig.pyright.setup({ capabilities = capabilities })
+		lspconfig.ts_ls.setup({ capabilities = capabilities })
+		lspconfig.clangd.setup({ capabilities = capabilities })
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = { globals = { 'vim' } },
+				},
+			},
+		})
+	end,
 }
