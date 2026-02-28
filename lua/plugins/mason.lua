23996d5b14984fc9b2b9a4d391d5c039d9fff04a
Trifalic47 <r00tgrep99@gmail.com>
2026-03-01 00:37:13 +0530

    Fix stylua error and consolidate formatting

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,30 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      local mason = require("mason")
+      local mason_tool_installer = require("mason-tool-installer")
+
+      -- Initialize Mason
+      mason.setup({
+        ui = {
+          border = "none",
+        },
+      })
+
+      -- Automatically install these tools
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "stylua", -- For formatting Lua (Fixes your current error)
+          "black", -- For Python formatting
+          "isort", -- For Python import sorting
+          "prettier", -- For JavaScript/Web formatting
+          "shfmt", -- For Bash scripting
+        },
+      })
+    end,
+  },
 }
--- lua/plugins/none.lua
+++ lua/plugins/none.lua
@@ -4,11 +4,8 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
+				-- Only keep tools that conform.nvim doesn't handle easily
 				null_ls.builtins.diagnostics.erb_lint,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.black,
 			},
 		})
 
